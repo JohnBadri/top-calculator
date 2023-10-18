0422cc62ca262689df015b9e83c5169a800658ad
JohnBadri <135007003+JohnBadri@users.noreply.github.com>
2023-10-18 16:59:06 +1100

    Completed step 1 and 2 of TOP Calculator

---
+++ script.js
@@ -0,0 +1,17 @@
+let fNumber = null;
+let sNumber = null;
+let operator = null;
+
+// Addition function
+let addition = (a, b) => a + b;
+
+// Subtraction function
+let subtraction = (a, b) => a - b;
+
+// multiplication function
+let multiplication = (a, b) => a * b;
+
+// Division function
+let division = (a, b) => a / b;
+
+console.log(division(12, 6));
